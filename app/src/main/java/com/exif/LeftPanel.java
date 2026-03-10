6ab1f8be59b23822a53796813f02d47f2b055665
szdav1 <szantodavid.mail@gmail.com>
2026-03-10 19:41:33 +0100

    Bug fixes.

--- app/src/main/java/com/exif/LeftPanel.java
+++ app/src/main/java/com/exif/LeftPanel.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.RandomAccessFile;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
